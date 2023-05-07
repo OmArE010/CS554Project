fcf2843d54bf38b9f062fb8001418720b36af1d3
Steven <steven.huu.ly@gmail.com>
2023-05-07 05:53:42 -0400

    messages tab

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,52 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
-const configRoutes = require("./routes");
-const cors = require('cors');
+// const app = express();
+// app.use(express.json());
+// const configRoutes = require("./routes");
+// const cors = require('cors');
 
-configRoutes(app);
+// configRoutes(app);
 
-app.use(cors());
+// app.use(cors());
 
-app.listen(4000, () => {
-	console.log("Server is now running on http://localhost:4000");
+const http = require('http').createServer(express);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('new client connected', socket.id);
+  socket.join("Room1");
+  socket.on('user_join', (name) => {
+    console.log('A user joined their name is ' + name);
+    socket.broadcast.emit('user_join', name);
+  });
+
+  socket.on('room', (roomName) => {
+    socket.leave();
+    socket.join(roomName);
+  });
+
+  socket.on('message', ({name, message, roomName}) => {
+    console.log(name, message, roomName, socket.id);
+    if (Object.values(socket.rooms).indexOf('Room1') > -1) {
+      console.log(`room1 found`);
+    }
+    if (Object.values(socket.rooms).indexOf('Room2') > -1) {
+      console.log(`room2 found`);
+    }
+    io.to(roomName).emit('message', {name, message, roomName});
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log('Disconnect Fired');
+  });
 });
+
+http.listen(4000, () => {
+  console.log(`listening on *:${4000}`);
+});
+
+
+// app.listen(4000, () => {
+// 	console.log("Server is now running on http://localhost:4000");
+// });
 
---
+++ client/src/components/Messages.js
@@ -0,0 +1,165 @@
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+import "../App.css";
+
+const fakeMessages = [{}, {}];
+
+function Messages() {
+  const [state, setState] = useState({ message: "", name: "", room: "" });
+  const [chat, setChat] = useState([]);
+  const [room, setRoom] = useState("Room1");
+
+  const socketRef = useRef();
+  const chatLogRef = useRef(null);
+
+  useEffect(() => {
+    socketRef.current = io("/");
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    socketRef.current.on("message", ({ name, message, roomName }) => {
+      setChat([...chat, { name, message, roomName }]);
+    });
+    socketRef.current.on("user_join", function (data) {
+      setChat([
+        ...chat,
+        {
+          name: "ChatBot",
+          message: `${data} has joined the ${room}`,
+          roomName: room,
+        },
+      ]);
+    });
+
+    if (chatLogRef.current) {
+      chatLogRef.current.scrollTop = chatLogRef.current.scrollHeight;
+    }
+
+    return () => {
+      socketRef.current.off("message");
+      socketRef.current.off("user_join");
+    };
+  }, [chat]);
+
+  const userjoin = (name) => {
+    socketRef.current.emit("user_join", name);
+  };
+
+  const onMessageSubmit = (e) => {
+    let msgEle = document.getElementById("message");
+    setState({ ...state, [msgEle.name]: msgEle.value });
+    socketRef.current.emit("message", {
+      name: state.name,
+      message: msgEle.value,
+      roomName: room,
+    });
+    e.preventDefault();
+    setState({ message: "", name: state.name, roomName: room });
+    msgEle.value = "";
+    msgEle.focus();
+  };
+
+  const renderChat = () => {
+    return chat.map(({ name, message, roomName }, index) => {
+      if (roomName === room) {
+        return (
+          <div key={index}>
+            <h3 id="chatm" className="chat-message">
+              {name}: <span>{message}</span>
+            </h3>
+          </div>
+        );
+      }
+      return null;
+    });
+  };
+
+  return (
+    <div class="messages">
+      {state.name && (
+        <div className="messages-container">
+          <div className="messages-bar">
+            <h2>Messages {socketRef.current.id}</h2>
+            <div className="messages-list">
+              <div className="messages-users">
+                {/* {users.map((user) => (
+                    <p key={user.socketID}>{user.userName}</p>
+                  ))} */}
+                <div>
+                  <button
+                    onClick={() => {
+                      setRoom("Room1");
+                      socketRef.current.emit("room", "Room1");
+                    }}
+                  >
+                    Join Room 1
+                  </button>
+                </div>
+                <div>
+                  <button
+                    onClick={() => {
+                      setRoom("Room2");
+                      socketRef.current.emit("room", "Room2");
+                    }}
+                  >
+                    Join Room 2
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="messages-body">
+            <div className="render-chat">
+              <p className="room-chat">{room}</p>
+              <div className="chat-log" ref={chatLogRef}>
+                {renderChat()}
+              </div>
+              <form className="send-box" onSubmit={onMessageSubmit}>
+                <div>
+                  <input
+                    name="message"
+                    id="message"
+                    variant="outlined"
+                    label="Message"
+                    placeholder="Write a message..."
+                  />
+                  <button type="submit">{">"}</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {!state.name && (
+        <form
+          className="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setState({ name: document.getElementById("username_input").value });
+            userjoin(document.getElementById("username_input").value);
+            // userName.value = '';
+          }}
+        >
+          <div className="form-group">
+            <label>
+              User Name:
+              <br />
+              <input id="username_input" />
+            </label>
+          </div>
+          <br />
+
+          <br />
+          <br />
+          <button type="submit"> Click to join</button>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default Messages;
