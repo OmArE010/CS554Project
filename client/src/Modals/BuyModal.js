ed87346942fb55ab8638bbf0f586971e3a422ea7
OmArE010 <omarebr01@gmail.com>
2023-05-09 19:51:36 -0400

    changes

--- client/src/Modals/BuyModal.js
+++ client/src/Modals/BuyModal.js
@@ -68,8 +68,8 @@ function BuyModal(props) {
                         <br></br>
                         <br></br>
                         <div className="modal-footer">
-                            <button className="btn btn-secondary" data-bs-dismiss="modal"> Cancel</button>
-                            <button className="btn btn-primary">Confirm</button>
+                            <button className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200" data-bs-dismiss="modal" type='button'> Cancel</button>
+                            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-non" type='submit'>Confirm</button>
                         </div>
                         </form>
                         </div>
